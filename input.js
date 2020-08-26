c60ba56853533b78604918be2d49951b78880be8
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-26 18:20:02 +0000

    Added WASD command keys to move the snake

--- input.js
+++ input.js
@@ -1,8 +1,10 @@
+let connection;
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -14,6 +16,14 @@ const setupInput = () => {
 const handleUserInput = (data) => {
   if (data === '\u0003') {
     process.exit();
+  } else if (data.toLowerCase() === 'w') {
+    connection.write("Move: up");
+  } else if (data.toLowerCase() === 'a') {
+    connection.write("Move: left");
+  } else if (data.toLowerCase() === 's') {
+    connection.write("Move: down");
+  } else if (data.toLowerCase() === 'd') {
+    connection.write("Move: right");
   }
 };
 
